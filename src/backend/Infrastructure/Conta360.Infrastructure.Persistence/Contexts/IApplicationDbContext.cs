a0528e134b89114d662aa1abe09f0327612880b2
galedifr <galedifr@gmail.com>
2025-06-07 03:39:46 +0200

    f3-ddd-Infrastructure modif

--- src/backend/Infrastructure/Conta360.Infrastructure.Persistence/Contexts/IApplicationDbContext.cs
+++ src/backend/Infrastructure/Conta360.Infrastructure.Persistence/Contexts/IApplicationDbContext.cs
@@ -1,7 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using Conta360.Domain.Entities;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
+using System.Collections.Generic;
 
 namespace Conta360.Infrastructure.Persistence.Contexts
 {
