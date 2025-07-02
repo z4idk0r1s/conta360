3c0cf692d18143b816dc4544b70bbc274aef9241
galedifr <galedifr@gmail.com>
2025-07-02 13:28:28 +0200

    f6-add excelfiscalprocessor

--- src/backend/Core/Conta360.Core/Common/PgcExtractorOptions.cs
+++ src/backend/Core/Conta360.Core/Common/PgcExtractorOptions.cs
@@ -5,25 +5,9 @@ namespace Conta360.Core.Common
     /// </summary>
     public class PgcExtractorOptions
     {
-        /// <summary>
-        /// URL del ZIP oficial de la taxonomía en ICAC.
-        /// </summary>
-        public string TaxonomyZipUrl { get; set; } =
-            "https://www.icac.gob.es/sites/default/files/pgc2007/v170/taxonomiaPGC2007_v1.7.0_20240101_r1-EIRL.zip";
-
-        /// <summary>
-        /// Nombre con que se guardará el ZIP localmente.
-        /// </summary>
-        public string ZipFileName { get; set; } = "taxonomiaPGC2007.zip";
-
-        /// <summary>
-        /// Carpeta donde se descargará y descomprimirá el ZIP.
-        /// </summary>
-        public string ExtractDirectory { get; set; } = "./Data/PGC";
-
-        /// <summary>
-        /// Si es true, al arrancar la aplicación descargará y procesará la taxonomía automáticamente.
-        /// </summary>
-        public bool EnableStartupDownload { get; set; } = true;
+        public string TaxonomyZipUrl { get; set; }
+        public string ZipFileName { get; set; }
+        public string ExtractDirectory { get; set; }
+        public bool EnableStartupDownload { get; set; }
     }
 }
