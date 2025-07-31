fa0380292f3eae3916717bc2e5f6f13f8f3719ce
galedifr <galedifr@gmail.com>
2025-08-01 00:20:20 +0200

    f10b-subveniones prev ok

--- src/SubvencionesApp/SubvencionesApp.Api/Clients/InfoSubvencionesApiClient.cs
+++ src/SubvencionesApp/SubvencionesApp.Api/Clients/InfoSubvencionesApiClient.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text.Json;
 
-namespace SubvencionesApp.Api.Client
+namespace SubvencionesApp.Api.Clients
 {
     public class InfoSubvencionesApiClient
     {
