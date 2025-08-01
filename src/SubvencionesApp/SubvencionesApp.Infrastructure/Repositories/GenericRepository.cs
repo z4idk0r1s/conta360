8edd2b5a28a4a15bd83ec6a19f1517b5a6469085
galedifr <galedifr@gmail.com>
2025-08-01 12:29:26 +0200

    f11-prev ok

--- src/SubvencionesApp/SubvencionesApp.Infrastructure/Repositories/GenericRepository.cs
+++ src/SubvencionesApp/SubvencionesApp.Infrastructure/Repositories/GenericRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SubvencionesApp.Domain.Interfaces;
+using SubvencionesApp.Infrastructure.Database;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
